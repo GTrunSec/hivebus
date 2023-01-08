0cabd2feb97eee0b13977a3d94a6dd840ff8be84
guangtao <gtrunsec@hardenedlinux.org>
2023-01-08 04:12:41 -0800

    init fcitx5😄️😄️😄️

--- nix/guangtao/secretProfiles.nix
+++ nix/guangtao/secretProfiles.nix
@@ -2,7 +2,7 @@
   guangtao = rec {
     openssl.desktop = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKTQ1V9mjPZY31h55Jl6nl7VBxl6cQgag9+s8kvJSEpZ NixOS-gtrun-14-05-2021";
     openssl.macos = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDI2uDp51HMggnfz4Zn5ky4gb/XVdQiJR7pZyb/LcCLx macOSdeMBP.attlocal.net-gtrun-14-05-2021";
-    openssl.new = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIASgkMbXa+U0AgchM3wkmjHPkB80Ypyl723Bxgt36Ytr Desktop-guangtao-06-01-2023";
+    openssl.new = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIChDNBEPNik+7vihFvjPPKF1Tm95cElPSWK6No5ED8Cz Desktop-guangtao-08-01-2023";
     openssh.authorizedKeys.keys = [
       openssl.macos
       openssl.desktop
---
+++ nix/i18n/entrypoints/fcitx5-init.sh
@@ -0,0 +1,2 @@
+cp -r $PRJ_ROOT/profiles/fcitx5/{profile,conf,config} ~/.config/fcitx5
+cp -r $PRJ_ROOT/profiles/fcitx5/themes ~/.local/share/fcitx5/
