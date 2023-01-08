6ccae3193ac1e27b3e0cf7d5c5ceb2200994e813
guangtao <gtrunsec@hardenedlinux.org>
2023-01-07 23:51:57 -0800

    fix logseq wayland

--- nix/utils/homeModules/notes.nix
+++ nix/utils/homeModules/notes.nix
@@ -3,7 +3,7 @@
     (
       pkgs.writeShellScriptBin "logseq-run" ''
          export XDG_DATA_DIRS=${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}:${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}:$XDG_DATA_DIRS
-        ${pkgs.appimage-run}/bin/appimage-run ${pkgs.guangtao-sources.logseq.src}
+        ${pkgs.appimage-run}/bin/appimage-run ${pkgs.guangtao-sources.logseq.src} --enable-features=UseOzonePlatform --ozone-platform=wayland
       ''
     )
   ];
