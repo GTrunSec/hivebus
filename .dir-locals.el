88e5804fda5e3ec1380a3213aaf4c13e2a619b07
guangtao <gtrunsec@hardenedlinux.org>
2023-03-27 22:17:55 -0700

    org-mode: sync

--- .dir-locals.el
+++ .dir-locals.el
@@ -6,9 +6,8 @@
          (eval . (setq org-attach-id-dir (concat (shell-command-to-string
                                                   "git rev-parse --show-toplevel | tr -d '\n'")
                                                  "/docs/org/attach/")))
-         (eval . (setq org-roam-db-location (concat (shell-command-to-string
-                                                     "git rev-parse --show-toplevel | tr -d '\n'")
-                                                    "/.cache/org-roam.db")))))
+         (eval . (setq org-roam-db-location (concat doom-cache-dir "org-hive.db")))))
+
  (org-mode . ((org-tanglesync-mode . t)))
  )
 ;;;docs/org-mode-locals.el ends here
