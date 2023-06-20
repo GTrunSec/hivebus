9030fd26e19e8cc97739ecbfeccf68b5ebc3a30e
guangtao <gtrunsec@hardenedlinux.org>
2023-06-20 19:41:44 +0000

    refactor(chatgpt-next-web): fix api forward

--- cells/hosts/nixosModules/tiangang/services/nginx/_config/chatgpt-next.nix
+++ cells/hosts/nixosModules/tiangang/services/nginx/_config/chatgpt-next.nix
@@ -4,6 +4,7 @@
   lib,
   ...
 }: let
+  cfg = config.services.chatgpt-next-web;
 in {
   imports = [
     inputs.cells.nixos.pops.exports.nixosModules.outputs.chatgpt-next-web
@@ -24,16 +25,31 @@ in {
       "chatgpt.zhangguangtao.org" = {
         enableACME = true;
         forceSSL = true;
-        extraConfig = ''
-          auth_basic "ChatGPT-web password";
-          auth_basic_user_file ${config.age.secrets.chatgpt-web-passwd.path};
-          proxy_read_timeout 1800s;
-          proxy_connect_timeout 1600s;
-          access_log off;
-        '';
         locations."/" = {
-          proxyPass = "http://127.0.0.1:3010";
+          proxyPass = "http://127.0.0.1:${toString cfg.port}";
           recommendedProxySettings = true;
+          extraConfig = ''
+            auth_basic "ChatGPT-web password";
+            auth_basic_user_file ${config.age.secrets.chatgpt-web-passwd.path};
+            proxy_read_timeout 1800s;
+            proxy_connect_timeout 1600s;
+            access_log off;
+            proxy_cache off;
+            proxy_buffering off;
+            chunked_transfer_encoding on;
+            tcp_nopush on;  # Turn on TCP NOPUSH option, disable Nagle algorithm
+            tcp_nodelay on;  # Turn on TCP NODELAY option, disable delay ACK algorithm
+          '';
+        };
+        # FIXME: https://github.com/Yidadaa/ChatGPT-Next-Web/issues/1339
+        locations."/api" = {
+          proxyPass = "http://127.0.0.1:${toString cfg.port}/api";
+          recommendedProxySettings = true;
+          extraConfig = ''
+            # suppressing the Authorization header
+            proxy_set_header Authorization "";
+            proxy_set_header X-Forwarded-User $remote_user;
+          '';
         };
       };
     };
